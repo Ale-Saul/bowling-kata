460aba8c966cd174561868169c05ebdb8d236543
Alejandro <alejandro.mont.cesp@gmail.com>
2024-03-18 18:49:56 -0400

    Se agrego la funcionalidad del spare

--- src/bowlingGame.test.js
+++ src/bowlingGame.test.js
@@ -13,17 +13,17 @@ describe("Jugar", () => {
 
   it("deberia devolver 0 de la funcion game", () => {
     rollMany(20, 0);
-    expect(game.getScore()).toBe(0);
+    expect(game.score()).toBe(0);
   });
   it("deberia devolver 20 de la funcion game", () => {
     rollMany(20, 1);
-    expect(game.getScore()).toBe(20);
+    expect(game.score()).toBe(20);
   });
-  /* it("deberia devolver el caso especial de 1 spare", () => {
+  it("deberia devolver el caso especial de 1 spare", () => {
     game.roll(5);
     game.roll(5);
     game.roll(3);
     rollMany(17, 0);
-    expect(game.getScore()).toBe(16);
-  }); */
+    expect(game.score()).toBe(16);
+  }); 
 });
