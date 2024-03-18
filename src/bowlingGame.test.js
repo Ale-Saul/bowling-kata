8707084e09b8fc9ef0e469970b83d3a28fdcd1f5
Alejandro <alejandro.mont.cesp@gmail.com>
2024-03-18 18:19:40 -0400

    Se refactorizo el test

--- src/bowlingGame.test.js
+++ src/bowlingGame.test.js
@@ -1,18 +1,22 @@
 import Game from "./bowlingGame.js";
 
 describe("Jugar", () => {
-  it("deberia devolver 0 de la funcion game", () => {
-    const game = new Game();
-    for (let i = 0; i < 20; i++) {
-      game.roll(0);
+  let game;
+  beforeEach(() => {
+    game = new Game();
+  });
+  const rollMany = (n, pins) => {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
     }
+  }
+
+  it("deberia devolver 0 de la funcion game", () => {
+    rollMany(20, 0);
     expect(game.getScore()).toBe(0);
   });
   it("deberia devolver 20 de la funcion game", () => {
-    const game = new Game();
-    for (let i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    rollMany(20, 1);
     expect(game.getScore()).toBe(20);
   });
 });
